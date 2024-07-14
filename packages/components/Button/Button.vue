8d172e29f954f0f6969f60f1c848c9286e715447
kokodayou2000 <1611601667@qq.com>
2024-07-15 00:37:43 +0800

    :tada: first commit

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+    name: "ErButton",    
+})
+</script>
+
+<template>
+    <button style="background-color: aqua;color: red;">this is a button</button>
+</template>
---
+++ packages/play/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+   <er-button></er-button>
+  </div>  
+</template>
+
+<style scoped>
+</style>
